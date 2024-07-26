082e98286c2ca881ac533dfbcca7f612c4607ee3
Roula Ghaleb <roula.ghaleb@lau.edu>
2024-07-26 22:48:10 +0300

    Time: 666 ms (36.24%), Space: 0B (100.00%) - LeetHub

---
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -0,0 +1,2 @@
+select DATE_FORMAT(trans_date, '%Y-%m') as month, country, COUNT(trans_date) as trans_count, SUM(state = 'approved') as approved_count, SUM(amount) as trans_total_amount, SUM(IF(state = 'approved', amount, 0)) approved_total_amount from Transactions
+group by month, country;
